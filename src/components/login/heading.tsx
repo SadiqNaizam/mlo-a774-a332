6a4e11267dd12572270132aa8fd09a8d384dfc49
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-24 13:39:35 +0530

    Updated code files: heading.tsx

---
+++ src/components/login/heading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Heading: React.FC = () => {
+  return (
+    <h1 className="text-2xl font-bold text-center">
+      Welcome
+    </h1>
+  );
+};
+
+export default Heading;
